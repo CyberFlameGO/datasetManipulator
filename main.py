77cf0834f14d187a9664a793cb39a3a0bddd3fa8
Aaron Lowe <cyberflameu@gmail.com>
2021-11-03 23:42:19 +1300

    a start

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""This will probably be a single-'class' project, its only goal is to sample a dataset"""
+from typing import Any, Union
 
-pd.read_csv
+import pandas as pd
+from pandas import DataFrame, Series
+from pandas.core.generic import NDFrame
+from pandas.io.parsers import TextFileReader
+
+cv_validated_df: Union[Union[TextFileReader, Series, DataFrame, None, NDFrame], Any] = pd.read_csv('validated.tsv', sep='\t')
+print(cv_validated_df)
