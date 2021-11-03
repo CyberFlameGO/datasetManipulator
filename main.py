0df5771de409ccfcebb6e791a37e74228286ee65
Aaron Lowe <cyberflameu@gmail.com>
2021-11-04 00:23:21 +1300

    final changes before deploying for test

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ from pandas import DataFrame, Series
 from pandas.core.generic import NDFrame
 from pandas.io.parsers import TextFileReader
 
-dataset: str = 'validated.tsv'
+dataset: str = './validated.tsv'
 sample_size: int = 50000
 random_state: int = 64
+dataframe_column: str = 'path'
+
+sample_output: str = "../sample/"
 
 dataframe: Union[Union[TextFileReader, Series, DataFrame, None, NDFrame], Any] = pd.read_csv(dataset, sep = '\t')
 sample = dataframe.sample(sample_size, replace = True, random_state = random_state)
-sample_paths = sample['path'].tolist()
+sample_paths = sample[dataframe_column].tolist()
 
 for i in sample_paths:
-    shutil.copy2(f"./clips/{i}", '../sample/')
+    shutil.copy2(f"./clips/{i}", sample_output)
     print(i)
