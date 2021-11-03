48991d7dc2add48002d29aa0510a4bfa1227b787
Aaron Lowe <cyberflameu@gmail.com>
2021-11-04 01:00:47 +1300

    minor changes
    this was a super basic 'script' (i think it's a script, no idea of the terminology. This could've been done way better

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ from typing import Any, Union
 import pandas as pd
 from pandas import DataFrame, Series
 from pandas.core.generic import NDFrame
-from pandas.io.parsers import TextFileReader
+import pandas.io.parsers
 
 dataset: str = './validated.tsv'
 sample_size: int = 50000
 random_state: int = 64
 dataframe_column: str = 'path'
 
-sample_output: str = "../sample/"
+sample_output: str = "./sample/"
 
-dataframe: Union[Union[TextFileReader, Series, DataFrame, None, NDFrame], Any] = pd.read_csv(dataset, sep = '\t')
-sample = dataframe.sample(sample_size, replace = True, random_state = random_state)
+dataframe: Union[Union[pandas.io.parsers.TextFileReader, Series, DataFrame, None, NDFrame], Any] = \
+    pd.read_csv(dataset, sep = '\t')
+sample: Union[Union[pandas.io.parsers.TextFileReader, Series, DataFrame, None, NDFrame], Any] = \
+    dataframe.sample(sample_size, replace = True, random_state = random_state)
 sample_paths = sample[dataframe_column].tolist()
 
+assert isinstance(sample_paths, object)
 for i in sample_paths:
     shutil.copy2(f"./clips/{i}", sample_output)
-
